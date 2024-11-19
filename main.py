5c5269359516e52f65bbad8d124b905a8c5391ca
aem2231 <mariojetullaji@gmail.com>
2024-11-19 19:32:37 +0000

    added a menue to choose options

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import os
 from pathlib import Path
 from generate_test_data import generate
-from wordcloud import wordcloud
 from simple_term_menu import TerminalMenu
+from most_said_wordcloud import most_said_wordcloud
+import typing
 
+# basic menue to configure options
 def main():
-    options = ["Yes", "No"]
-    test_data_menu = TerminalMenu(options, title="Use test data?")
-    #menu_entry_index is the choice made by the user. for example, if they chouse "Yes", menu entry index will equal 1
-    menu_entry_index = menu.show() 
-    if menu_entry_index = 1:
-        new_test_data = 
+    use_test_data: bool = False
+    use_blacklist: bool = False
+    options: list[str] = ["Yes", "No"]
 
-main()
+    test_data_menu = TerminalMenu(options, title="Use test data?")
+    menu_entry_index: int = test_data_menu.show() # menu_entry_index is the choice made by the user.                                                   
+    if menu_entry_index == 0:                     # for example, if they chose "Yes", menu entry index will equal 0
+        use_test_data = True
+        if os.path.exists("test_data"): 
+            overwite_test_data_menu = TerminalMenu(options, title="A test data directory already exists. \nWould you like to generate new data?")
+            menu_entry_index = overwite_test_data_menu.show()
+            if menu_entry_index == 0:
+                generate() 
+                os.system('cls' if os.name == 'nt' else 'clear')
+        else:
+            generate()
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+    blacklist_menu = TerminalMenu(options, title="Use blacklist?")
+    menu_entry_index: int = blacklist_menu.show()
+    if menu_entry_index == 0:
+        use_blacklist = True
+        print("Blackist will be used.\n")
+
+    
+    most_said_wordcloud(use_blacklist, use_test_data)
+        
+if __name__ == "__main__":
+    main()
