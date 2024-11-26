e6a6543c073a956b562084da0a8140b06b7463a3
aem2231 <mariojetullaji@gmail.com>
2024-11-26 12:17:02 +0000

    finishing word stats

--- modules/word_stats.py
+++ modules/word_stats.py
@@ -3,14 +3,39 @@ from pathlib import Path
 import re
 from typing import Union
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud
 from tqdm import tqdm
 import os
 import time
-from operator import itemgetter
+import itertools
 from most_said_wordcloud import *
+from better_profanity import profanity 
 
-def word_counts(use_test_data) -> dict:
+def count_messages() -> str:
+    rootdir: Path = os.getcwd()
+    data_folder: str = "test_data"
+    folders: list = [folder for folder in rootdir.iterdir() if folder.is_dir()]
+    messages: list = []
+
+
+    for folder in tqdm(folders, desc="Processing Folders", unit="folder"):
+        json_file = folder / "messages.json"
+        
+        if json_file.exists():
+            try:
+                with open(json_file, "r", encoding="utf-8") as file:
+                    data = json.load(file)
+                
+                for dict in data:
+                    message: str = dict['Contents']
+                    if message not in messages:
+                        messages.append(message)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON in {json_file}: {e}")
+        else:
+            print(f"No messages.json found in {folder}")
+    return messages
+    
+def count_words(use_test_data) -> dict:
     start_time = time.time()
 
     if use_test_data:
@@ -27,14 +52,34 @@ def word_counts(use_test_data) -> dict:
 
     for folder in tqdm(folders, desc="Processing Folders", unit="folder"):
         process_folder(folder, blacklist, word_counts)
-    ""
-    return sorted(word_counts, key=itemgetter(1))
+    return dict(sorted(word_counts.items(), key=lambda x:x[1], reverse=True)) # I dont know why this works but it does 
 
-def percentage_swearing() -> int:
-    pass
+def count_percentage_offensiveness(messages, message_count) -> int:
+    offensive_message_count: int = 0
+    for message in messages:
+        censored_message: str = profanity.censor(message)
+        if censored_message != message:
+            offensive_message_count += 1
+
+    return (offensive_message_count/message_count) 
 
 def word_use_over_time() -> dict:
     pass
 
-print(word_counts(True))
+def grammer_accuracry() -> int:
+    pass
+
+def word_stats() -> None:
+    messages = count_messages()
+    message_count = len(messages)
+    word_counts = count_words(True)
+    top5_words: dict[str: int] = dict(itertools.islice(word_counts.items(), 5))
+    percententage_offensiveness: int = count_percentage_offensiveness(messages, message_count)
+    
+
+    
+
+
+
+word_stats()
 
